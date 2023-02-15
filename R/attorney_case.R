b568d7435dfcd341e6f6e6e0e404d1dd7781c305
palesl <patrick.leslie@anu.edu.au>
2023-02-15 14:03:14 +1100

    attorney_case and error checks

---
+++ R/attorney_case.R
@@ -0,0 +1,14 @@
+#' Attorney bio data for HCA cases 1995-2019
+#'
+#' Biographical data from all attorneys appearing before the High Court of Australia in full hearings, 1995-2019
+#'
+#' @format ## `justice_bio`
+#' A tibble with 1755 rows and 436 columns:
+#' \describe{
+#'   \item{justice}{HCDB justice ID number}
+#'   \item{caseNumber}{HCA docket number}
+#'   \item{caseName}{name of the case}
+#'   ...
+#' }
+#' @source Robinson, Z., Leslie, P. (High Court Database)
+"attorney_case"
