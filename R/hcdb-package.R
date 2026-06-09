de9d8d29c0d9e8ec5029847837498f5a58cefad4
palesl <patrick.leslie@anu.edu.au>
2026-06-09 21:18:37 +1000

    try

---
+++ R/hcdb-package.R
@@ -0,0 +1,19 @@
+#' @details
+#' The hcdb package provides four datasets. Each has a corresponding codebook
+#' vignette with full variable documentation:
+#'
+#' | Dataset | Codebook |
+#' |---|---|
+#' | [justice_decision] | `vignette("justice_decision_codebook")` |
+#' | [justice_decision] | `vignette("justice_decision_core_codebook")` |
+#' | [special_leave] | `vignette("special_leave_codebook")` |
+#' | [justice_bio] | `vignette("justice_bio_codebook")` |
+#' | [advocate_case] | `vignette("advocate_case_codebook")` |
+#'
+#' To browse all codebooks at once: `browseVignettes("hcdb")`
+#'
+#' @references Leslie, P., Robinson, Z., Smyth, R., & Jacobi, T. (2026). Supporting comparative
+#'   studies of judicial behavior: Introducing the Australian High Court Database. *Journal of
+#'   Law & Empirical Analysis*, *3*(1), 251–263. \doi{10.1177/2755323X261438254}
+#' @keywords internal
+"_PACKAGE"
